d566d3a1e21b2612d4b30cb5871a071fd7d7a940
mark79-github <52877024+mark79-github@users.noreply.github.com>
2021-04-17 19:13:48 +0300

    make responsive NavBar to work properly ..

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React, {useContext, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FiMenu, FiX} from 'react-icons/fi';
 
-import './NavBar.css';
+import styles from './NavBar.module.css';
 
 import AuthContext from '../../contexts';
 
@@ -10,8 +10,12 @@ const NavBar = () => {
     const {isLoggedIn, displayName, userId} = useContext(AuthContext);
     const [open, setOpen] = useState(false);
 
-    const handleClick = () => {
-        setOpen(!open);
+    const handleClick = (e) => {
+        if (e.target.tagName.toLowerCase() === 'svg' || e.target.tagName.toLowerCase() === 'line') {
+            setOpen(!open);
+            return;
+        }
+        setOpen(false);
     };
 
     const closeMenu = () => {
@@ -20,43 +24,43 @@ const NavBar = () => {
 
     return (
 
-        <header className="header">
-                <nav className="navbar">
-                    <Link to="/" className="nav-link">
-                        All Posts
-                    </Link>
-                    <div onClick={handleClick} className="nav-icon">
-                        {open ? <FiX/> : <FiMenu/>}
-                    </div>
-                    <ul className={open ? 'nav-links active' : 'nav-links'}>
-                        {isLoggedIn
-                            ?
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/post/create" className="nav-link" onClick={closeMenu}>
-                                        Add Post
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to={`/?author=${userId}`} className="nav-link" onClick={closeMenu}>
-                                        Own Posts
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/user/logout" className="nav-link" onClick={closeMenu}>
-                                        Logout, {displayName}
-                                    </Link>
-                                </li>
-                            </>
-                            :
-                            <li className="nav-item">
-                                <Link to="/user/sign" className="nav-link" onClick={closeMenu}>
-                                    Sign
+        <header className={styles.header}>
+            <nav className={styles.navbar}>
+                <Link to="/" className={styles.link} onClick={closeMenu}>
+                    All Posts
+                </Link>
+                <div onClick={(e) => handleClick(e)} className={styles.icon} id="nav-icon">
+                    {open ? <FiX/> : <FiMenu/>}
+                </div>
+                <ul className={open ? `${styles.links} ${styles.active}` : `${styles.links}`}>
+                    {isLoggedIn
+                        ?
+                        <>
+                            <li className={styles.item}>
+                                <Link to="/post/create" className={styles.link} onClick={closeMenu}>
+                                    Add Post
                                 </Link>
                             </li>
-                        }
-                    </ul>
-                </nav>
+                            <li className={styles.item}>
+                                <Link to={`/?author=${userId}`} className={styles.link} onClick={closeMenu}>
+                                    Own Posts
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link to="/user/logout" className={styles.link} onClick={closeMenu}>
+                                    Logout, {displayName}
+                                </Link>
+                            </li>
+                        </>
+                        :
+                        <li className={styles.item}>
+                            <Link to="/user/sign" className={styles.link} onClick={closeMenu}>
+                                Sign
+                            </Link>
+                        </li>
+                    }
+                </ul>
+            </nav>
         </header>
     );
 };
