11dc6a1ad4848f80a483b54c64121a6230c96547
Martin Dimitrov <martin_dimitrov@abv.bg>
2021-04-27 16:31:57 +0300

    show loader on showing comments ..

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from 'react';
+import {useContext, useEffect, useRef, useState} from 'react';
 
 import styles from './Comment.module.css';
 
@@ -6,31 +6,46 @@ import AuthContext from '../../contexts';
 
 import * as userService from '../../services/userService';
 import Avatar from "../Icons/Avatar";
-
+import Loader from "react-loader-spinner";
 
 const Comment = ({data, onDeleteComment}) => {
     const {_id, index, comment, user} = data;
     const authContext = useContext(AuthContext);
-    const [authorDisplayName, setAuthorDisplayName] = useState('');
-    const [authorAvatarImgURL, setAuthorAvatarImgUrl] = useState('');
+
+    const [loading, setLoading] = useState(true);
+
+    const authorDisplayName = useRef('');
+    const authorAvatarImgURL = useRef('');
 
     const handleClick = () => onDeleteComment(_id);
 
     useEffect(() => {
+
         const getUserData = () => {
             userService.getById(user)
                 .then(res => {
-                    setAuthorDisplayName(res.displayName);
-                    setAuthorAvatarImgUrl(res.avatarImageUrl);
+                    authorDisplayName.current = res.displayName;
+                    authorAvatarImgURL.current = res.avatarImageUrl;
+                    setLoading(false);
                 });
         }
+
         getUserData();
     }, [user]);
 
+    if (loading) {
+        return (
+            <article className={styles.container}>
+                <Loader type="Rings" color="white" height={80} width={80}/>
+            </article>
+        )
+    }
+
     return (
         <article className={styles.container}>
-            <span className={styles.span}>#{index}</span> <span className={styles.span}>{authorDisplayName}</span>
-            <Avatar img={authorAvatarImgURL}/>
+            <span className={styles.span}>#{index}</span>
+            <span className={styles.span}>{authorDisplayName.current}</span>
+            <Avatar img={authorAvatarImgURL.current}/>
             {
                 authContext.userId === user
                     ? <i className={`${styles.icon} far fa-trash-alt`} onClick={handleClick}/>
