fc1e6950544c1eb87eb9ab32a0a69ed04ad9cf6b
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-11-21 05:43:05 +0200

    Refactor the FormOrder component to functional format

--- client/src/components/FormOrder/FormOrder.js
+++ client/src/components/FormOrder/FormOrder.js
@@ -1,85 +1,31 @@
-import {useEffect, useState} from 'react';
-
 import styles from './FormOrder.module.css';
 
 const selectOptions = [
-    {
-        label: "Last published",
-        value: "publishedAt",
-    },
-    {
-        label: "Most likes",
-        value: "likes",
-    },
-    {
-        label: "Most comments",
-        value: "comments",
-    },
-    {
-        label: "Most views",
-        value: "visits",
-    },
+    {label: "Last published", value: "publishedAt"},
+    {label: "Most likes", value: "likes"},
+    {label: "Most comments", value: "comments"},
+    {label: "Most views", value: "visits"},
 ];
 
-const FormOrder = (props) => {
-    const {onOrderChange, disabled} = props;
-    const [orderBy, setOrderBy] = useState('publishedAt');
-
-    const selectChangeHandler = (event) => {
-        setOrderBy(event.target.value);
-    }
-
-    useEffect(() => {
-        onOrderChange(orderBy);
-    }, [orderBy, onOrderChange]);
-
+const FormOrder = ({onOrderChange, disabled, orderBy}) => {
     return (
         <section className={styles.container}>
             <form className={styles.form}>
-                <select className={styles.select} value={orderBy}
-                        onChange={selectChangeHandler} disabled={disabled}>
+                <select
+                    className={styles.select}
+                    value={orderBy}
+                    onChange={(e) => onOrderChange(e.target.value)}
+                    disabled={disabled}
+                >
                     {selectOptions.map((option, index) => (
-                        <option key={index} value={option.value}>{option.label}</option>
+                        <option key={index} value={option.value}>
+                            {option.label}
+                        </option>
                     ))}
                 </select>
             </form>
         </section>
     );
 }
-
-// class FormOrder extends Component {
-//
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             orderBy: 'publishedAt'
-//         };
-//     }
-//
-//     selectChangeHandler = (event) => {
-//         this.setState({orderBy: event.target.value});
-//     }
-//
-//     componentDidUpdate(prevProps, prevState, snapshot) {
-//         if (prevState.orderBy !== this.state.orderBy) {
-//             this.props.onOrderChange(this.state.orderBy);
-//         }
-//     }
-//
-//     render() {
-//         return (
-//             <section className={styles.container}>
-//                 <form className={styles.form}>
-//                     <select className={styles.select} value={this.state.orderBy}
-//                             onChange={this.selectChangeHandler} disabled={this.props.disabled}>
-//                         {selectOptions.map((option, index) => (
-//                             <option key={index} value={option.value}>{option.label}</option>
-//                         ))}
-//                     </select>
-//                 </form>
-//             </section>
-//         );
-//     }
-// }
 
 export default FormOrder;
