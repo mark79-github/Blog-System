6a90d9bd78da3f24d9063a5d74b77ff3c9cf1676
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-11-23 06:19:44 +0200

    Remove commented-out code and optimize date handling

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,12 +2,10 @@ import {api} from '../utils/globals'
 import {request, requestForm} from '../utils/data';
 
 export const register = (data) => {
-
     return requestForm(api.auth.registerURL, 'POST', data);
 };
 
 export const login = (email, password) => {
-
     const data = {email, password}
     return request(api.auth.loginURL, 'POST', data);
 };
