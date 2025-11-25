1ea51f24fc6500808fd8f2ef2b2e6f93f91ea2a1
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-11-25 05:14:02 +0200

    Remove commented-out code

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,7 @@
-import React from 'react';
-// import Navigation from '../Navigation';
 import NavBar from "../NavBar";
 
 const Header = () => {
     return (
-        // <Navigation/>
         <NavBar/>
     )
 }
