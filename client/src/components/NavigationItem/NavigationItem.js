774d0deaa84e3ef66d0d6b640b5aca223d2915f8
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-11-25 05:41:03 +0200

    Simplify NavigationItem by removing unused clickHandler logic.

--- client/src/components/NavigationItem/NavigationItem.js
+++ client/src/components/NavigationItem/NavigationItem.js
@@ -4,20 +4,13 @@ import {Link} from 'react-router-dom';
 import styles from './NavigationItem.module.css';
 
 const NavigationItem = (props) => {
-
-    const clickHandler = () => {
-        props.onSearch({author: props.userId})
-    }
-
     return (
         <li className={styles.item}>
-            {
-                !props.userId
-                    ? <Link className={styles.link} to={props.linkTo} >{props.children}</Link>
-                    : <Link className={styles.link} to={{pathname:'/', author:props.linkTo}} onClick={clickHandler}>{props.children}</Link>
-            }
+            <Link className={styles.link} to={props.linkTo}>
+                {props.children}
+            </Link>
         </li>
     );
-}
+};
 
 export default NavigationItem;
